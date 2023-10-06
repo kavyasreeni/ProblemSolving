d742e4321b53911aed699b26e95d65f693361435
kavya <kavya.maddu@gmail.com>
2023-10-06 14:34:18 +0900

    :Implemented python for loop

---
+++ loops_python.py
@@ -0,0 +1,6 @@
+# Task
+# The provided code stub reads and integer, n , from STDIN. For all non-negative integers i<n, print i^2 .
+# 1st Method
+number = int(input('Enter number: '))
+for n in range(number): 
+    print(n*n)
