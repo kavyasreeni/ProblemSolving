31ba89a8a10cefc0f4bb9ac81cf04df8a196935d
kavya <kavya.maddu@gmail.com>
2023-10-06 14:06:13 +0900

    :logic add

--- Arithmetic.py
+++ Arithmetic.py
@@ -4,3 +4,9 @@
 # The first line contains the sum of the two numbers.
 # The second line contains the difference of the two numbers (first - second).
 # The third line contains the product of the two numbers.
+
+a = int(input('Enter number: '))
+b = int(input('Enter number: '))
+print(a+b)
+print(a-b)
+print(a*b)
