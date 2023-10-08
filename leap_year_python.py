1739a0c3a52763f16459ffe672fab44d4585594a
kavya <kavya.maddu@gmail.com>
2023-10-08 18:32:35 +0900

    :modified logic

--- leap_year_python.py
+++ leap_year_python.py
@@ -9,13 +9,11 @@
 
 def is_leap(year):
     leap=False
-    if (year%4==0 and not (year%100==0)) :
+    if year%400==0:
         return True
-    elif year%400==0:
-        return leap
+    elif (year%4==0 and not (year%100==0)) :
+        return True
     return leap
 
 year_pass = is_leap(int(input('Enter number: ')))
 print(year_pass)
-
-
