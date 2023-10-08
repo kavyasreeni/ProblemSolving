73a348d527e51dc8a0f347ba5c72692db82454a5
kavya <kavya.maddu@gmail.com>
2023-10-08 18:22:43 +0900

    :Implemented leap year in python

---
+++ leap_year_python.py
@@ -0,0 +1,21 @@
+# In the Gregorian calendar, three conditions are used to identify leap years:
+
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+# Task
+# Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+# Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
+
+def is_leap(year):
+    leap=False
+    if (year%4==0 and not (year%100==0)) :
+        return True
+    elif year%400==0:
+        return leap
+    return leap
+
+year_pass = is_leap(int(input('Enter number: ')))
+print(year_pass)
+
+
