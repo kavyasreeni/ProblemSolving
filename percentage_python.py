069e97be87e9e5b641f67a723e038f0d8ddda9f6
kavya <kavya.maddu@gmail.com>
2023-11-08 10:24:27 +0900

    :Implemented average in python

---
+++ percentage_python.py
@@ -0,0 +1,14 @@
+# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+# Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    student_score = student_marks.get(query_name)
+    query_name=(sum(student_score)/len(student_score))
+    print("average of student marks is: ",(query_name))
