09845c720254e86ea284117a4752068987043c77
kavya <kavya.maddu@gmail.com>
2023-11-08 10:41:12 +0900

    :Nested list python

--- nested_list.py
+++ nested_list.py
@@ -1,17 +1,25 @@
 # Given the names and grades for each student in a class of N students,
 # store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
 
-deatils_of_people_list = []
-for _ in range(int(input())):
-    name = input()
-    score = float(input())
-    name_score_list = []
-    name_score_list.append(name)
-    name_score_list.append(score)
-    deatils_of_people_list.append(name_score_list)
-    # print(deatils_of_people_list)
-    # deatils_of_people_list.sort()
-    sorted(deatils_of_people_list)
-    # sorted_list = deatils_of_people_list[-2]
-    print(deatils_of_people_list)
-    
+# Python code to sort the lists using the second element 
+# of sublists inplace way to sort, use of third variable
+def Sort(sub_li):
+   
+    l = len(sub_li)
+     
+    for i in range(0, l):
+        for j in range(0, l-i-1):
+             
+            if (sub_li[j][1] > sub_li[j + 1][1]):
+                tempo = sub_li[j]
+                sub_li[j] = sub_li[j + 1]
+                sub_li[j + 1] = tempo
+     
+    return sub_li
+ 
+# Input list
+sub_li = [['rishav', 10], ['akash', 5], ['ram', 20], ['gaurav', 15]]
+
+# Printing the list 
+print(Sort(sub_li))
+
